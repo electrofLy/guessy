ca4e270a456da29c45775f1d6a69345481e213ba
Erik Mom <Erik.Mom@ortec-finance.com>
2023-06-14 22:21:48 +0200

    Refactored end component to use signals

--- src/app/pages/play/end/end.component.ts
+++ src/app/pages/play/end/end.component.ts
@@ -1,35 +1,36 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { combineLatest, interval, map, startWith, tap } from 'rxjs';
+import { interval, map, startWith, tap } from 'rxjs';
 import { TranslocoModule } from '@ngneat/transloco';
 import { MatCardModule } from '@angular/material/card';
 import { AsyncPipe, CommonModule, NgIf, NgOptimizedImage } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { PlayService } from '../play.service';
 import { SettingsService } from '../../../core/services/settings.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-end',
-  template: ` <ng-container *ngIf="view$ | async as view">
+  template: ` <ng-container>
     <img
       class="self-center mb-4"
-      *ngFor="let country of [view.country]"
-      [ngClass]="{ invert: settingsService.themeSignal() === 'dark' && view.type === 'SHAPE' }"
-      [ngSrc]="view.type === 'SHAPE' ? country.shapeUrl : country.flagUrl"
-      [height]="view.type === 'SHAPE' ? 200 : 150"
+      *ngIf="countrySignal() as country"
+      [ngClass]="{ invert: settingsService.themeSignal() === 'dark' && typeSignal() === 'SHAPE' }"
+      [ngSrc]="typeSignal() === 'SHAPE' ? country.shapeUrl : country.flagUrl"
+      [height]="typeSignal() === 'SHAPE' ? 200 : 150"
       priority="true"
       width="200"
       alt="country-flag-am"
     />
-    <p class="text-justify">
-      <ng-container *ngIf="view.isGuessed === true; else failed">
-        <span data-test="success-guess">{{ 'endSuccess' | transloco : [view.country.name] }}</span>
+    <p class="text-justify" *ngIf="countrySignal() as country">
+      <ng-container *ngIf="isGuessedSignal() === true; else failed">
+        <span data-test="success-guess">{{ 'endSuccess' | transloco : [country.name] }}</span>
       </ng-container>
       <ng-template #failed>
-        <span data-test="fail-guess">{{ 'endFailed' | transloco : [view.country.name] }}</span>
+        <span data-test="fail-guess">{{ 'endFailed' | transloco : [country.name] }}</span>
       </ng-template>
     </p>
     <p class="text-justify">
-      {{ 'endDetails' | transloco : [view.countdown] }}
+      {{ 'endDetails' | transloco : [countdownSignal()] }}
     </p>
   </ng-container>`,
   styles: [],
@@ -51,50 +52,47 @@ import { SettingsService } from '../../../core/services/settings.service';
 export class EndComponent {
   playService = inject(PlayService);
   settingsService = inject(SettingsService);
-  isGuessed$ = this.playService.isGuessed$;
-  country$ = this.playService.country$;
+
+  isGuessedSignal = toSignal(this.playService.isGuessed$, { initialValue: false });
+  countrySignal = toSignal(this.playService.country$);
+  typeSignal = toSignal(this.playService.type$, { initialValue: 'FLAG' });
   today = new Date();
-  countdown$ = interval(1000).pipe(
-    startWith(0),
-    map(() => {
-      return (
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        new Date(
-          this.today.getFullYear(),
-          this.today.getMonth(),
-          this.today.getDate() + 1
+  countdownSignal = toSignal(
+    interval(1000).pipe(
+      startWith(0),
+      map(() => {
+        return (
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
-        ) - new Date()
-      );
-    }),
-    tap((remainingTime) => {
-      if (remainingTime <= 0) {
-        location.reload();
-      }
-    }),
-    map((remainingTime) => {
-      // Convert remaining time to hours, minutes, and seconds
-      const hours = Math.floor(remainingTime / (1000 * 60 * 60))
-        .toString()
-        .padStart(2, '0');
-      const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60))
-        .toString()
-        .padStart(2, '0');
-      const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000)
-        .toString()
-        .padStart(2, '0');
+          new Date(
+            this.today.getFullYear(),
+            this.today.getMonth(),
+            this.today.getDate() + 1
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+          ) - new Date()
+        );
+      }),
+      tap((remainingTime) => {
+        if (remainingTime <= 0) {
+          location.reload();
+        }
+      }),
+      map((remainingTime) => {
+        // Convert remaining time to hours, minutes, and seconds
+        const hours = Math.floor(remainingTime / (1000 * 60 * 60))
+          .toString()
+          .padStart(2, '0');
+        const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60))
+          .toString()
+          .padStart(2, '0');
+        const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000)
+          .toString()
+          .padStart(2, '0');
 
-      // Display the remaining time
-      return `${hours}:${minutes}:${seconds}`;
-    })
+        // Display the remaining time
+        return `${hours}:${minutes}:${seconds}`;
+      })
+    )
   );
-
-  view$ = combineLatest({
-    isGuessed: this.isGuessed$,
-    country: this.country$,
-    countdown: this.countdown$,
-    type: this.playService.type$
-  });
 }
